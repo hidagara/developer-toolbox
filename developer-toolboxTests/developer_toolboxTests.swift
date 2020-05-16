a91fd3161ff987d20a93b80efbb4d84785ab1d76
hidagara <hidagara@gmail.com>
2020-05-16 20:41:19 +0300

    Initial Commit

---
+++ developer-toolboxTests/developer_toolboxTests.swift
@@ -0,0 +1,34 @@
+//
+//  developer_toolboxTests.swift
+//  developer-toolboxTests
+//
+//  Created by Roman Guseynov on 16.05.2020.
+//  Copyright © 2020 Roman Guseynov. All rights reserved.
+//
+
+import XCTest
+@testable import developer_toolbox
+
+class developer_toolboxTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
